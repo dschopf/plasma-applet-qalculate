e6ef122b652a6dbc1b966e8b2f90153c5a72b9a8
Daniel Schopf <schopf.dan@gmail.com>
2021-01-02 12:56:07 +0100

    Update CI to use Ubuntu 20.10 instead of 19.10

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -10,12 +10,12 @@ docker build -t paq_ubuntu:18.04 -f ubuntu_18.04.dockerfile .
 docker tag paq_ubuntu:18.04 schopfdan/paq_ubuntu:18.04
 docker push schopfdan/paq_ubuntu:18.04
 
-docker pull ubuntu:19.10
-docker build -t paq_ubuntu:19.10 -f ubuntu_19.10.dockerfile .
-docker tag paq_ubuntu:19.10 schopfdan/paq_ubuntu:19.10
-docker push schopfdan/paq_ubuntu:19.10
-
 docker pull ubuntu:20.04
 docker build -t paq_ubuntu:20.04 -f ubuntu_20.04.dockerfile .
 docker tag paq_ubuntu:20.04 schopfdan/paq_ubuntu:20.04
 docker push schopfdan/paq_ubuntu:20.04
+
+docker pull ubuntu:20.10
+docker build -t paq_ubuntu:20.10 -f ubuntu_20.10.dockerfile .
+docker tag paq_ubuntu:20.10 schopfdan/paq_ubuntu:20.10
+docker push schopfdan/paq_ubuntu:20.10
