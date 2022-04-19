143f1fe5563dc805ae7e750423940f9767ebd1e9
Daniel Schopf <schopf.dan@gmail.com>
2022-04-19 18:14:31 +0200

    Add support for 22.04 github actions build

---
+++ .github/actions/ubuntu_22.04/build_plugin.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -ev
+rm -rf build
+mkdir build
+cd build
+cmake .. \
+	-DCMAKE_INSTALL_PREFIX=/usr \
+	-DCMAKE_BUILD_TYPE=Debug \
+	-DCMAKE_CXX_FLAGS=-Werror \
+	-DKDE_INSTALL_LIBDIR=lib \
+	-DKDE_INSTALL_USE_QT_SYS_PATHS=ON
+make
